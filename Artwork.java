73f8a0c149f9bfd18facbf8b35a7f87c5060a9ab
Muhammad Jahanzaib Iqbal <sp24-bse-076@cuilahore.edu.pk>
2024-09-20 15:45:33 +0500

    Add files via upload

---
+++ Artwork.java
@@ -0,0 +1,55 @@
+class Artwork {
+    // Fields
+    private String title;
+    private int year;
+    private Artist artist;
+
+    public Artwork(String title, int year, Artist artist) {
+        this.title = title;
+        this.year = year;
+        this.artist = artist;
+    }
+
+    public Artwork(String title, int year) {
+        this.title = title;
+        this.year = year;
+        this.artist = new Artist("Unknown Artist");
+    }
+
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    
+    public void displayInformation() {
+        System.out.println("Title: " + title + ", Year: " + year + ", Artist: " + artist.getName());
+    }
+
+   
+    public Artwork shallowCopy() {
+        return new Artwork(this.title, this.year, this.artist);
+    }
+
+    
+    public Artwork deepCopy() {
+        return new Artwork(this.title, this.year, new Artist(this.artist.getName()));
+    }
+}
---
+++ Run.java
@@ -0,0 +1,46 @@
+public class Run {
+    public static void main(String[] args) {
+        
+        Artist artist1 = new Artist("Leonardo da Vinci");
+
+        
+        Artwork artwork1 = new Artwork("Mona Lisa", 1503, artist1);
+
+        
+        Artwork artwork2 = new Artwork("Unknown Masterpiece", 2023);
+
+        
+        artwork2.setTitle("Modern Art");
+        artwork2.setYear(2022);
+
+        
+        System.out.println("Original Artworks:");
+        artwork1.displayInformation();
+        artwork2.displayInformation();
+
+        
+        Artwork shallowCopyArtwork = artwork1.shallowCopy();
+        Artwork deepCopyArtwork = artwork1.deepCopy();
+
+        
+        System.out.println("\nBefore modifying artist:");
+        System.out.println("Original Artwork:");
+        artwork1.displayInformation();
+        System.out.println("Shallow Copy:");
+        shallowCopyArtwork.displayInformation();
+        System.out.println("Deep Copy:");
+        deepCopyArtwork.displayInformation();
+
+        
+        artist1 = new Artist("Leonardo Updated");  // Changing the artist name
+
+       
+        System.out.println("\nAfter modifying artist:");
+        System.out.println("Original Artwork:");
+        artwork1.displayInformation();
+        System.out.println("Shallow Copy:");
+        shallowCopyArtwork.displayInformation();
+        System.out.println("Deep Copy:");
+        deepCopyArtwork.displayInformation();
+    }
+}
